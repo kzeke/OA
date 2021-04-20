e74e32d93d43ffbcc9351b82d6cf39874e3771ec
kzeke <kurtzek3@gmail.com>
2021-04-20 16:14:22 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function burgerIcon() {
+    var menulist = document.getElementById("site-branding");
+    var burger = document.getElementById("nav-icon3");
+    menulist.classList.toggle("show");
+    burger.classList.toggle("open");
+    // alert('show');
+}
